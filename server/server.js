ba64478581d9d0249d8271bf3e56246fbd30d8b4
HenryC0416 <henrychen0416@gmail.com>
2026-04-03 01:30:17 -0400

    Setup backend structure

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Quizrr backend is running!");
+});
+
+
+app.listen(3001, () => {
+  console.log("Server running on http://localhost:3001");
+});
